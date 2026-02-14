83d0b037c6cb1c9f6357d4e91c6b544002f227c5
chesrel <172009994+chesrel@users.noreply.github.com>
2026-02-14 18:54:18 +0100

    added support for multiple dependencies when subscribing a callback; cleaned up the folder to store lib files only; introduced modules

--- jsx-to-js.js
+++
@@ -1,6 +0,0 @@
-(async function () {
-    const testComponent = await (await fetch('./test.jsx')).text();
-
-    const rootElement = document.getElementById('root');
-    rootElement.innerHTML = testComponent;
-})()
--- test.jsx
+++
@@ -1,7 +0,0 @@
-<div style={{
-    padding: '20px',
-    backgroundColor: 'dodgerblue',
-    color: 'white'
-}}>
-    <h1>Test component</h1>
-</div>
